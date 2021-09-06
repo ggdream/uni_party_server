be8da86cec12216a9dd517176589262f372d26cf
ggdream <gdream@yeah.net>
2021-09-06 14:31:55 +0800

    登录session

--- cache/error.go
+++ cache/error.go
@@ -2,6 +2,7 @@ package cache
 
 import (
 	"errors"
+
 	"github.com/go-redis/redis/v8"
 )
 
@@ -11,4 +12,6 @@ var (
 	AddItAlreadyErr = errors.New("the user has been followed")
 	DelItAlreadyErr = errors.New("the user has been unfollowed")
 	MustGEZeroErr   = errors.New("values of offset and number must be ge zero")
+	NotFoundErr     = errors.New("not found")
+	MatchFailedErr  = errors.New("match failed")
 )
--- cache/session.go
+++ cache/session.go
@@ -1,5 +1,37 @@
 package cache
 
+import (
+	"fmt"
+	"time"
 
-//存一个session id为key，uid为value作为session存储。
-//存一份uid作为key，session id为值，用来重复调用使上次未失效session失效
+	"github.com/go-redis/redis/v8"
+)
+
+// Session string
+type Session struct{}
+
+func (s Session) joinKey(uid uint) string {
+	return fmt.Sprintf("user:session:%d", uid)
+}
+
+// Set 按uid存储session
+func (s Session) Set(uid uint, session string) error {
+	return client.Set(s.joinKey(uid), session, 7*24*time.Hour)
+}
+
+// Verify 鉴别uid和session的对应关系
+func (s Session) Verify(uid uint, session string) (bool, error) {
+	value, err := client.Get(s.joinKey(uid)).Result()
+	if err != nil {
+		if err == redis.Nil {
+			return false, NotFoundErr
+		} else {
+			return false, err
+		}
+	}
+
+	if value != session {
+		return false, MatchFailedErr
+	}
+	return true, nil
+}
